300700a3d69d61f2d2307bc44d86dfceffbc80f1
62656E <bet.travis36@live.com>
2024-11-28 09:38:18 +0000

    Enhance sidebar inputs for SYBR Green qPCR Calculator to clarify gene and sample entries

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,17 @@ st.title('SYBR Green qPCR Calculator. All volumes given in uL. The calculator as
 # Sidebar with user inputs
 st.sidebar.header('User Inputs')
 reaction_vol = st.sidebar.selectbox('Reaction volume (uL)', [10, 20])
-no_genes = st.sidebar.number_input('Number of genes')
-no_samples = st.sidebar.number_input('Number of samples per gene')
+no_genes = st.sidebar.number_input('Number of genes (primer pairs)')
+no_samples = st.sidebar.number_input('Number of samples per gene (primer pair)')
+
+# Create output dataframe
+data = pd.DataFrame(columns = ["Gene", "YSBVol", "DNAVol"])
+
+# Fetch gene/primer pair names
+gene_names = [] 
+for i in range(no_genes):
+    gene_name = st.sidebar.text_input(f'Gene {i+1} name', f'Gene {i+1}')
+
 
 
 
