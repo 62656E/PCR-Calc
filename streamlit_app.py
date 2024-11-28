2229218ce3ceab71392ba293b9056467c0a1d180
62656E <bet.travis36@live.com>
2024-11-28 09:31:33 +0000

    Update title and sidebar inputs for SYBR Green qPCR Calculator

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
 # Import necessary libraries
 import streamlit as st
 from io import BytesIO
+import pandas as pd
 
 # Title Streamlit app
-st.title('SYBR Green qPCR Calculator')
+st.title('SYBR Green qPCR Calculator. All volumes given in uL. The calculator assumes 40X yellow sample buffer is being used.')
 
 # Sidebar with user inputs
 st.sidebar.header('User Inputs')
 reaction_vol = st.sidebar.selectbox('Reaction volume (uL)', [10, 20])
 no_genes = st.sidebar.number_input('Number of genes')
-no_samples = st.sidebar.number_input('Number of samples')
+no_samples = st.sidebar.number_input('Number of samples per gene')
+
+
+
+
+
 
