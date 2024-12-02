3a0b247a3f8495ad0e364382b0cee89186eea917
62656E <bet.travis36@live.com>
2024-12-02 14:36:38 +0000

    Refactor reagent volume calculations to use YSB volumes and update display and download functionality

--- pages/2_Reagent_Volume_Calculator.py
+++ pages/2_Reagent_Volume_Calculator.py
@@ -10,13 +10,9 @@ with open("data.pkl", "rb") as f:
     data = pkl.load(f)
 
 # Unpack data
-reagent_vols = data["reagent_vols"]
 master_mix_vols = data["master_mix_vols"]
 ysb_vols = data["ysb_vols"]
 samples = data["samples"]
-
-print("RVC Reagent Volumes " + str(reagent_vols))
-print("RVC Master Mix Volumes " + str(master_mix_vols))
 
 # Title page
 st.title("Reagent Volumes")
@@ -25,7 +21,7 @@ st.markdown(
 )
 
 # Create data frames
-reagent_vol_df = pd.DataFrame(
+ysb_vols_df = pd.DataFrame(
     columns=[range(1, samples + 1)],
     index=["Template DNA", "YSB", "Total"],
 )
@@ -37,24 +33,24 @@ master_mix_vols_df = pd.DataFrame(
 
 # Populate data frames
 for sample in range(1, samples + 1):
-    reagent_vol_df[sample] = reagent_vols[sample]
+    ysb_vols_df[sample] = ysb_vols[sample]
     master_mix_vols_df[sample] = master_mix_vols[sample]
 
 # Multiply all reagent volumes by 10% to account for pipetting error
-reagent_vol_df *= 1.1
+ysb_vols_df *= 1.1
 master_mix_vols_df *= 1.1
 
 # Display both dataframes
-st.write("Reagent Volumes")
-st.write(reagent_vols)
+st.write("YSB and Sample DNA Volumes")
+st.write(ysb_vols_df)
 st.write("Master Mix Volumes")
 st.write(master_mix_vols)
 
 # Add download links for reagent volumes
 
-reagent_buffer = BytesIO()
-reagent_vols.to_csv(reagent_buffer, index=False)
-reagent_buffer.seek(0)
+ysb_temp_buffer = BytesIO()
+ysb_vols_df.to_csv(ysb_temp_buffer, index=False)
+ysb_temp_buffer.seek(0)
 
 master_buffer = BytesIO()
 master_mix_vols.to_csv(master_buffer, index=False)
@@ -62,7 +58,7 @@ master_buffer.seek(0)
 
 st.download_button(
     label="Download Plate Layout",
-    data=reagent_buffer,
+    data=ysb_temp_buffer,
     file_name="Template_YSB_Volumes.csv",
     mime="text/csv",
 )
